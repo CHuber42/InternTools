d21a81b90747464f9d08068a047b00387e336893
Chris Huber <CHuber42.Work@Gmail.com>
2020-08-19 10:49:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import image from "./img/guf_logo.png";
+import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
 export default App;
+
+
+
